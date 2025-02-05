29f3a2a4768dbbc1ee21da70290d62a9bd2bbac0
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-05 14:40:43 -0500

    Finished Tutorials.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Windows Tutorial
+
+            //int a = 7;
+            //int b = 4;
+            //int c = 3;
+            //int d = (a + b) / c;
+            //int e = (a + b) % c;
+            //Console.WriteLine($"quotient: {d}");
+            //Console.WriteLine($"remainder: {e}");
+
+            //int max = int.MaxValue;
+            //int min = int.MinValue;
+            //Console.WriteLine($"The range of integers is {min} to {max}");
+
+            //int what = max + 3;
+            //Console.WriteLine($"An example of overflow: {what}");
+
+            //double a = 5;
+            //double b = 4;
+            //double c = 2;
+            //double d = (a + b) / c;
+            //Console.WriteLine(d);
+
+            //decimal min = decimal.MinValue;
+            //decimal max = decimal.MaxValue;
+            //Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            //double a = 1.0;
+            //double b = 3.0;
+            //Console.WriteLine(a / b);
+
+            //decimal c = 1.0M;
+            //decimal d = 3.0M;
+            //Console.WriteLine(c / d);
+
+            //CHALLENGE
+
+            double radius = 2.50;
+            double area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine(Math.Round(area, 2));
+
+
+
+
+
+
         }
     }
 }
